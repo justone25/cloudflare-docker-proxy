4c2d3c4cc24b3d0e5511955c5e8db654354e91e8
justone25 <ly763247862@163.com>
2024-07-27 12:43:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.littlecarp.xyz": dockerHub,
+  "quay.littlecarp.xyz": "https://quay.io",
+  "gcr.littlecarp.xyz": "https://gcr.io",
+  "k8s-gcr.littlecarp.xyz": "https://k8s.gcr.io",
+  "k8s.littlecarp.xyz": "https://registry.k8s.io",
+  "ghcr.littlecarp.xyz": "https://ghcr.io",
+  "cloudsmith.littlecarp.xyz": "https://docker.cloudsmith.io",
+  "ecr.littlecarp.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.littlecarp.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
